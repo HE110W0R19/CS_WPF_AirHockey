f1ca362783f7bf753b18ef0463a02495306afb62
Unknown <103126971+HE110W0R19@users.noreply.github.com>
2022-11-17 02:59:28 +0300

    No Changes

--- Class_Work/Class_Work/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Class_Work/Class_Work/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace Class_Work {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Class_Work;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Class_Work;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
